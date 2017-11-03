f0cb80ea8263f6d1eaf3a2f5a110f59befc8161a
Dineshkarthik <dinesh.kartoon@gmail.com>
2017-11-03 16:55:22 +0530

    UPD: Version

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 
+VERSION = (0, 0, 1)
+__version__ = VERSION
+__versionstr__ = '.'.join([str(v) for v in VERSION])
+
 install_requires = ['beautifulsoup4>=4.3', 'requests>=2.7']
 
 setup(name='open_graph',
-      version='0.1',
+      version=__versionstr__,
       description='Python module to parse Open Graph metadata of Web Pages',
       author='Dineshkarthik R',
       author_email='dineshkarthik.r@gmail.com',
